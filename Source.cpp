fa7edb86982bc05dce4cafa3e92954edfa6a6994
Nurzhan Myrzatayev <klemptan@gmail.com>
2021-06-23 10:06:17 +0600

    add main func

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <fstream>
+#include <vector>
+#include <Windows.h>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	SetConsoleCP(1251); SetConsoleOutputCP(1251);
+	srand(time(NULL));
+
+}
