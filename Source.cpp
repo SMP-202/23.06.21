8b4c9578fab19c258d1f1b6ed31bafcd6107b03c
klemptan <50325916+klemptan@users.noreply.github.com>
2021-06-23 11:26:26 +0600

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -12,9 +12,7 @@ int main()
 	srand(time(NULL));
 	cout << "Hello, world!" << endl;
 	cout << "Jhon Smith" << endl;
-<<<<<<< HEAD
 	cout << "Sultan" << endl;
-=======
 	cout << "Hello" << endl;
 	cout << "Julia" << endl;
 	cout << "Hi there!" << endl;
